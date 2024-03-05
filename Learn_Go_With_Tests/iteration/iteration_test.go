6bfa038a869b4e0b4406e705e380bbb5a7192f54
Dante Kelly <dante@dantekelly.com>
2024-03-04 20:30:45 -0600

    Add example and expand tests

--- Learn_Go_With_Tests/iteration/iteration_test.go
+++ Learn_Go_With_Tests/iteration/iteration_test.go
@@ -1,18 +1,38 @@
 package iteration
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestRepeat(t *testing.T) {
-	got := Repeat("a", 5)
-	want := "aaaaa"
+	t.Run("repeating 5 times", func(t *testing.T) {
+		got := Repeat("a", 5)
+		want := "aaaaa"
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+
+	t.Run("repeating 7 times", func(t *testing.T) {
+		got := Repeat("a", 7)
+		want := "aaaaaaa"
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
 }
 
 func BenchmarkRepeat(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		Repeat("a", 5)
 	}
+}
+
+func ExampleRepeat() {
+	repeated := Repeat("a", 5)
+	fmt.Println(repeated)
+	// Output: aaaaa
 }
