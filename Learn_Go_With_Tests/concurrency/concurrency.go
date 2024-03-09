97557140a2baa0c6c344e406ea61f9c86f84898f
Dante Kelly <dante@dantekelly.com>
2024-03-08 21:08:54 -0600

    Add concurrency implementation and tests

---
+++ Learn_Go_With_Tests/concurrency/concurrency.go
@@ -0,0 +1,26 @@
+package main
+
+type WebsiteChecker func(string) bool
+type result struct {
+	string
+	bool
+}
+
+func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
+	results := make(map[string]bool)
+	resultChannel := make(chan result)
+
+	for _, url := range urls {
+		// This go version fixed the issue of the same url going in, but we will still add the usual fix.
+		go func(u string) {
+			resultChannel <- result{u, wc(url)}
+		}(url)
+	}
+
+	for i := 0; i < len(urls); i++ {
+		r := <-resultChannel
+		results[r.string] = r.bool
+	}
+
+	return results
+}
